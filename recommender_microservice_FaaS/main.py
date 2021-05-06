16c064227947117afb5408f465197cfa7705a8ed
TawabG <tghorbandi@gmail.com>
2021-05-06 21:37:12 +0200

    testing POST

--- recommender_microservice_FaaS/main.py
+++ recommender_microservice_FaaS/main.py
@@ -3,21 +3,21 @@ from resources.recommendation import SimpleRecommender
 def get_recommendation(request):
     print(request.path)
     from flask import abort
-    if request.method == 'GET':
+    if request.method == 'POST':
         # Alternative method
         # request_args = request.args
         # movie_name = request_args['movie_name']
 
 
-        #return jsonify({'testing': 'recommender'}), 200
+        return jsonify({'testing': 'recommender'}), 200
 
-        request_movie = request.args.get('movie_name')
-        request_overviews = request.args.get('overviews')
-        request_titles = request.args.get('title')
+        # request_movie = request.args.get('movie_name')
+        # request_overviews = request.args.get('overviews')
+        # request_titles = request.args.get('title')
 
-        return SimpleRecommender.recommend(movie_name=request_movie,
-                                           movie_overviews=request_overviews,
-                                           movie_titles=request_titles)
+        # return SimpleRecommender.recommend(movie_name=request_movie,
+        #                                    movie_overviews=request_overviews,
+        #                                    movie_titles=request_titles)
 
     else:
         return abort(404)
